ca9a4a0b5b92c65883aea2f9ee973c4a389ce4b0
Jianny <jianny.lee96@gmail.com>
2025-02-18 00:18:34 +0900

    [chore] contextLoads() 수정

--- src/test/java/rilakkuma/rilakcozy/RilakcozyApplicationTests.java
+++ src/test/java/rilakkuma/rilakcozy/RilakcozyApplicationTests.java
@@ -2,8 +2,10 @@ package rilakkuma.rilakcozy;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@ActiveProfiles("test")
+@SpringBootTest(classes = RilakcozyApplicationTests.class)
 class RilakcozyApplicationTests {
 
 	@Test
